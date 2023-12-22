30d21e908595959245aa8aaae803afb815d8339a
Sandipshrestha1 <sandipshrestha2050@gmail.com>
2023-12-22 23:08:52 +0545

    front page ui

---
+++ quiz_app/lib/gradient_container.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+//final Color color1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.purple,
+      ),
+      child: Column(
+        // crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('assets/images/quiz-logo.png', width: double.infinity, height: 350,),
+          const SizedBox(height: 60),
+          const Text(
+            "Learn Flutter the fun Way!",
+            style: TextStyle(fontSize: 28.0, color: Colors.white),
+          ),
+          const SizedBox(height: 40),
+          ElevatedButton(  
+            onPressed: () {},
+            child: const Text("Start Quiz", style: TextStyle(fontSize: 18.0),),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ quiz_app/lib/main.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+import 'package:quiz_app/gradient_container.dart';
+void main() {
+  runApp(
+
+   const  MaterialApp(
+      home: 
+    Scaffold(body:GradientContainer(
+//
+//Colors.pink,
+
+    ) ),)
+  );
+}

cbee4b69b7355cf64c6509db3713622f0ad3b194
Sandipshrestha1 <sandipshrestha2050@gmail.com>
2023-12-25 14:02:45 +0545

    quiz ui improved

--- quiz_app/lib/quiz.dart
+++ quiz_app/lib/quiz.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/questions_screen.dart';
 import 'start_screen.dart';
+
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
 
@@ -9,46 +10,49 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-// we cann't use var because var restrict   activescreen 
+  var activeScreen = 'start-screen';
+
+// we cann't use var because var restrict   activescreen
 //  var activeScreen = const StartScreen();
- Widget?  activeScreen ;
- // StartScreen(switchScreen);
+  //Widget?  activeScreen ;
+  // StartScreen(switchScreen);
 
-@override
-void initState() {
-activeScreen = StartScreen(switchScreen);
+// @override
+// void initState() {
+// activeScreen = StartScreen(switchScreen);
 
-    super.initState();
+//     super.initState();
+//   }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+//  QuestionsScreen();
+    });
   }
-
-void switchScreen() {
-setState(() {
-activeScreen =const  QuestionsScreen();
-
-
-});
-
-}
-
 
   @override
   Widget build(BuildContext context) {
+    var screenWidget = StartScreen(switchScreen);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color.fromRGBO(46, 6, 46, 1),
-                  Color.fromRGBO(226, 19, 226, 1),
-                ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromRGBO(46, 6, 46, 1),
+                Color.fromRGBO(226, 19, 226, 1),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
-            child: activeScreen,
-            ),
+          ),
+          child: activeScreen == 'start-screen'
+              ? StartScreen(switchScreen)
+              : const QuestionsScreen(),
+        ),
       ),
     );
   }
